77dcfa413e6079317d723e472de417fbb0290758
Addon Factory template <addonfactory@splunk.com>
2020-09-16 18:03:41 -0400

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -90,7 +90,7 @@ do
         echo Repository is existing
         hub api orgs/$REPOORG/teams/products-shared-services-all/repos/$REPOORG/$REPO --raw-field 'permission=maintain' -X PUT || true
         hub api orgs/$REPOORG/teams/productsecurity/repos/$REPOORG/$REPO --raw-field 'permission=read' -X PUT || true
-        hub api /repos/$REPOORG/$REPO --raw-field 'visibility=${REPOVISIBILITY}' -X PATCH || true
+        hub api repos/$REPOORG/$REPO --raw-field 'visibility=${REPOVISIBILITY}' -X PATCH || true
 
         if [ ! -d "$REPO" ]; then
             #hub clone $REPOORG/$REPO work/$REPO
